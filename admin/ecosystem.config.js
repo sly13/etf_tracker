7ff7a85f14ee7536a892908d76f030b02b1fa460
skin-forge <zhozefinal9nta@rambler.ru>
2025-09-12 01:00:05 +0300

    fix

--- admin/ecosystem.config.js
+++ admin/ecosystem.config.js
@@ -2,18 +2,23 @@ module.exports = {
 	apps: [
 		{
 			name: "etf-admin-panel",
-			script: "node_modules/.bin/react-scripts",
+			script: "npm",
 			args: "start",
 			cwd: "/var/www/etf_tracker_app/admin",
 			env: {
 				PORT: 3065,
 				NODE_ENV: "development",
-				REACT_APP_API_URL: "https://etf-flow.vadimsemenko.ru/api"
-			},
+				REACT_APP_API_URL: "http://localhost:3066/api"
+			}
+		},
+		{
+			name: "etf-admin-panel-serve",
+			script: "npx",
+			args: "serve -s build -l 3065",
+			cwd: "/var/www/etf_tracker_app/admin",
 			env_production: {
 				PORT: 3065,
-				NODE_ENV: "production",
-				REACT_APP_API_URL: "https://etf-flow.vadimsemenko.ru/api"
+				NODE_ENV: "production"
 			}
 		}
 	]
