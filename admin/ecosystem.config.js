d6b75fe3bcba2b47f79c2e58e05a0456eb221bc6
skin-forge <zhozefinal9nta@rambler.ru>
2025-09-12 00:42:06 +0300

    edit eco

--- admin/ecosystem.config.js
+++ admin/ecosystem.config.js
@@ -7,7 +7,13 @@ module.exports = {
 			cwd: "/var/www/etf_tracker_app/admin",
 			env: {
 				PORT: 3065,
-				NODE_ENV: "development"
+				NODE_ENV: "development",
+				REACT_APP_API_URL: "https://etf-flow.vadimsemenko.ru/api"
+			},
+			env_production: {
+				PORT: 3065,
+				NODE_ENV: "production",
+				REACT_APP_API_URL: "https://etf-flow.vadimsemenko.ru/api"
 			}
 		}
 	]
